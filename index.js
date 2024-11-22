ff9f75f6e084732bd2d4349e9835df41b475c69d
Anandakrishnan H <anandak19h@gmail.com>
2024-11-23 00:35:56 +0530

    email-js added

--- index.js
+++ index.js
@@ -10,20 +10,45 @@ function closemenu() {
 
 // Email send
 
-const scriptURL =
-  "https://script.google.com/macros/s/AKfycbwKySksuW6OqeuimKAaZLvXY8w-QkL90cuTnwF5XyP5sOeGO41SV_VmkWj8itBH6Iy3dA/exec";
-const form = document.forms["submit-to-google-sheet"];
-const msg = document.getElementById("msg");
+function submitForm() {
+  const namePattern = /^[a-zA-Z]+$/;
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => {
-      msg.innerHTML = "Message sent successfully";
-      setTimeout(function () {
-        msg.innerHTML = "";
-      }, 5000);
-      form.reset();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  // Validation checks
+  if (!namePattern.test(name) || name == "" || name == null) {
+    alert("Enter a valid name");
+    return;
+  }
+  if (!emailPattern.test(email) || email == "" || email == null) {
+    alert("Enter a valid email address");
+    return;
+  }
+  if (message == "" || message == null) {
+    alert("Enter a message");
+    return;
+  }
+
+  const params = {
+    name: name,
+    email: email,
+    message: message,
+  };
+
+  const serviceId = "service_et7ex4m";
+  const templateId = "template_ycprxnk";
+
+  emailjs
+    .send(serviceId, templateId, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      document.getElementById("msg").innerHTML =
+        "I got your message 😊 Thank you";
     })
-    .catch((error) => console.error("Error!", error.message));
-});
+    .catch((err) => console.log(err));
+}
